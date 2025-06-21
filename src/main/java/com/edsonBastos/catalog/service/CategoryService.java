85e6b36753b8172bce4056b1056c7435d069b0ec
edsonbastos2 <edsonbastos2@hotmail.com>
2025-06-21 19:58:25 -0300

    add service category

--- src/main/java/com/edsonBastos/catalog/service/CategoryService.java
+++ src/main/java/com/edsonBastos/catalog/service/CategoryService.java
@@ -1,4 +1,33 @@
 package com.edsonBastos.catalog.service;
 
+import com.edsonBastos.catalog.controller.request.CategoryRequest;
+import com.edsonBastos.catalog.controller.response.CategoryResponse;
+import com.edsonBastos.catalog.entity.Category;
+import com.edsonBastos.catalog.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+    private final CategoryRepository repository;
+
+    public List<CategoryResponse> getAll() {
+        return repository.findAll();
+    }
+
+    public CategoryResponse saveCategory(CategoryRequest category) {
+        return repository.save(category);
+    }
+
+    public Optional<CategoryResponse> getCategoryById(Long id){
+        return repository.findById(id);
+    }
+
+    public void deleteCategory(Long id){
+        repository.deleteById(id);
+    }
 }
