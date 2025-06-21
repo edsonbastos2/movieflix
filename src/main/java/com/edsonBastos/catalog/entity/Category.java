c02ed751f0b8caeb4c56e7661a0621ebb047766b
edsonbastos2 <edsonbastos2@hotmail.com>
2025-06-21 19:56:29 -0300

    feat:add entity category

--- src/main/java/com/edsonBastos/catalog/entity/Category.java
+++ src/main/java/com/edsonBastos/catalog/entity/Category.java
@@ -1,4 +1,21 @@
 package com.edsonBastos.catalog.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
 }
