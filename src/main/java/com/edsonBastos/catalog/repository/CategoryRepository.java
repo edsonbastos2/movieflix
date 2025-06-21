2414e2d6c1b0457d91c3a034f7dc729088760f59
edsonbastos2 <edsonbastos2@hotmail.com>
2025-06-21 19:57:55 -0300

    add repository category

--- src/main/java/com/edsonBastos/catalog/repository/CategoryRepository.java
+++ src/main/java/com/edsonBastos/catalog/repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.edsonBastos.catalog.repository;
 
-public interface CategoryRepository {
+import com.edsonBastos.catalog.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
