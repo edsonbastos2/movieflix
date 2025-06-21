b4e4c58cf118fc8c367e3109df3ea60a8edc9233
edsonbastos2 <edsonbastos2@hotmail.com>
2025-06-21 19:57:08 -0300

    feat:add mapper category

--- src/main/java/com/edsonBastos/catalog/mapper/CategoryMapper.java
+++ src/main/java/com/edsonBastos/catalog/mapper/CategoryMapper.java
@@ -1,4 +1,25 @@
 package com.edsonBastos.catalog.mapper;
 
+import com.edsonBastos.catalog.controller.request.CategoryRequest;
+import com.edsonBastos.catalog.controller.response.CategoryResponse;
+import com.edsonBastos.catalog.entity.Category;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class CategoryMapper {
+
+    public static Category toCategory(CategoryRequest request) {
+        return Category
+                .builder()
+                .name(request.name())
+                .build();
+    }
+
+    public static CategoryResponse toCategoryResponse(Category request) {
+        return CategoryResponse
+                .builder()
+                .id(request.getId())
+                .name(request.getName())
+                .build();
+    }
 }
